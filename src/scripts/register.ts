5cd89b2fee0ee2e1e16bbf39b41509f7967847b4
ARGI-BERRI <124055639+ARGI-BERRI@users.noreply.github.com>
2023-02-10 19:35:51 +0900

    chore: split src/ into app/ and scripts/

---
+++ src/scripts/register.ts
@@ -0,0 +1,10 @@
+// import { REST, Routes } from "discord.js";
+// 
+// const TOKEN = process.env["DISCORD_TOKEN"]
+// const CLIENT_ID = process.env["DISCORD_CLIENT_ID"]
+// 
+// const rest = new REST({ version: "10"}).setToken(TOKEN)
+// 
+// const data = await rest.put(Routes.applicationCommands(CLIENT_ID), {
+//     body: command
+// })
