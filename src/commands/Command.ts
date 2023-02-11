0155c2c9fdf30c0cf1bad9109fa003e6997d0018
ARGI-BERRI <124055639+ARGI-BERRI@users.noreply.github.com>
2023-02-11 15:49:51 +0900

    chore: add comment

--- src/commands/Command.ts
+++ src/commands/Command.ts
@@ -1,13 +1,21 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
+/**
+ * This interface represents the structure of Discord's slack command.
+ */
 export interface Command {
   /**
-   * data is used for command registration
+   * `data` is used for command registration.
+   *
+   * This defines what arguments will be accepted by this bot.
    */
   data: SlashCommandBuilder;
 
   /**
-   * execute() is used for command execution
+   * `execute()` is used for command execution.
+   *
+   * When `ChatInputCommandInteraction` is triggered, this `execute()`
+   * will handle a corresponding command.
    */
   execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
 }
